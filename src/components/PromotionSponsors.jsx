fea9aff57edde23c1adb00aa3af5ef182ceac0dc
ahmad oa <ouladaouida@gmail.com>
2023-07-18 01:53:34 +0100

    added the sponsors infinite carousel

--- src/components/PromotionSponsors.jsx
+++ src/components/PromotionSponsors.jsx
@@ -1,7 +1,32 @@
+import { sponsorImages } from "../utils/sponsorLogos";
+
 export const Promotion = () => {
   return <div>Promotion Component</div>;
 };
 
 export const Sponsors = () => {
-  return <div>Sponsors Component</div>;
+  return (
+    <div className="my-7 flex items-center justify-center mx-3">
+      <div className="w-[200%] h-24 py-2 border-t border-b border-stone-300 overflow-hidden relative">
+        {/* 2. */}
+        <div className="w-[200%] flex items-center h-20 justify-around absolute left-0 animate gap-5 md:gap-20 animate">
+          {/* 3 */}
+          {sponsorImages.map((i) => {
+            return (
+              <div className="flex justify-center items-start w-[9rem]">
+                <img src={i.img} />
+              </div>
+            );
+          })}
+          {sponsorImages.map((i) => {
+            return (
+              <div className="flex justify-center items-start w-[9rem]">
+                <img src={i.img} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ src/utils/sponsorLogos.js
@@ -0,0 +1,17 @@
+import Comet from "../assets/comet.png";
+import Bona from "../assets/bona.png";
+import Abnormal from "../assets/abnormal.png";
+import Stark from "../assets/stark.png";
+import Precisio from "../assets/precisio.png";
+
+export const sponsorImages = [
+  {
+    img: Comet,
+  },
+  {
+    img: Bona,
+  },
+  { img: Abnormal },
+  { img: Stark },
+  { img: Precisio },
+];
