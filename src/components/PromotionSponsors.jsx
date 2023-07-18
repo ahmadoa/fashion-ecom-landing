e322b593e90ad5607f9d0a7dad9e21c9d904fb8d
ahmad oa <ouladaouida@gmail.com>
2023-07-18 02:56:10 +0100

    added promotion infinite carousel

--- src/components/PromotionSponsors.jsx
+++ src/components/PromotionSponsors.jsx
@@ -1,16 +1,63 @@
+import { useState, useEffect } from "react";
 import { sponsorImages } from "../utils/sponsorLogos";
+import DiscountIcon from "../assets/discount.png";
 
 export const Promotion = () => {
-  return <div>Promotion Component</div>;
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <div className="my-7 flex items-center justify-center mx-3">
+      <div className="w-[200%] h-[5.5rem] py-1 border-t border-b border-dashed border-stone-300 overflow-hidden relative">
+        <div className="w-[200%] flex items-center h-20 justify-around absolute left-0 gap-5 md:gap-20 animateInvert">
+          {Array.from(
+            {
+              length:
+                windowSize[0] <= 500
+                  ? 4
+                  : windowSize[0] > 500 && windowSize[0] <= 1100
+                  ? 8
+                  : windowSize[0] > 1100 && windowSize[0] <= 1400
+                  ? 8
+                  : 14,
+            },
+            (_, i) => (
+              <div className="flex justify-center items-center w-[9rem] gap-2 md:gap-5">
+                <img
+                  src={DiscountIcon}
+                  className="h-10 w-10 sm:h-12 sm:w-12 md:h-16 md:w-16"
+                />
+                <div className="font-semibold text-Textprimary text-sm sm:text-base md:text-[2rem] whitespace-nowrap">
+                  Discount 20%
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const Sponsors = () => {
   return (
     <div className="my-7 flex items-center justify-center mx-3">
-      <div className="w-[200%] h-24 py-2 border-t border-b border-stone-300 overflow-hidden relative">
-        {/* 2. */}
-        <div className="w-[200%] flex items-center h-20 justify-around absolute left-0 animate gap-5 md:gap-20 animate">
-          {/* 3 */}
+      <div className="w-[200%] h-20 md:h-24 md:py-2 border-t border-b border-stone-300 overflow-hidden relative">
+        <div className="w-[200%] flex items-center h-20 justify-around absolute left-0 animate gap-8 sm:gap-5 md:gap-20 animate">
           {sponsorImages.map((i) => {
             return (
               <div className="flex justify-center items-start w-[9rem]">
